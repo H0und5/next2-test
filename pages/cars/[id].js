d4c5936f5ce6f5390173a5bc125784f269b5d570
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-07 19:34:34 -0500

    -- Observation -- Something about getStaticPaths which tells Next how many and which dynamic pages are needed to be rendered

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,15 +1,47 @@
 import { useRouter } from "next/router";
 
-export default function Car() {
+import Head from "next/head";
+
+import Image from "next/image";
+
+export default function Car({ car }) {
 
   const router = useRouter();
   const { id } = router.query;
 
-  return <h1>Hello {id}</h1>
+  return (<>
+
+  <Head>
+
+    <title>{car.color} {car.id}</title>
+
+  </Head>
+  
+    <h1>Hello {id}</h1>
+    <Image src={car.image} alt={`Picture of a ${id}`} />
+
+  </>)
+
+
 
 }
 
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+
+
+  const req = await fetch(`https://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+
+  }
+
+}
+
+
+
+export async function getStaticPaths() {
 
 }
